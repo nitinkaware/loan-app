d87a442a69d4c7b0166f90a3f727a2d2d5bbc0ca
Nitin Kaware <nitin.kaware1@gmail.com>
2022-08-09 14:37:42 +0530

    Add register and login route functionality.

---
+++ app/Http/Controllers/API/LoginController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class LoginController extends Controller
+{
+    public function index(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+            'device_name' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        return [
+            'token' => $user->createToken($request->device_name)->plainTextToken,
+        ];
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6',
+            'confirm_password' => 'required|string|min:6|same:password',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return [
+            'message' => 'User created successfully.',
+        ];
+    }
+}
